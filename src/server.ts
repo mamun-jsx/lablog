583bb5648d2ef2a0acc0cfd662a185c8db53c8e8
almamun528 <ayapths@gmail.com>
2026-01-19 01:02:25 +0600

    configer prisma

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import app from "./app";
+import { prisma } from "./lib/prisma";
 
 const PORT = process.env.PORT || 3001;
-function server() {
+async function server() {
+  try {
+    await prisma.$disconnect();
+  } catch (error) {
+        console.error(error);
+        await prisma.$disconnect();
+        process.exit(1);
+  }
   app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
   });
