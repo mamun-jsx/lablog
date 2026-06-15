f08edffff321cb94a1beab855e883fafdc51b0b9
almamun528 <ayapths@gmail.com>
2026-06-15 22:43:02 +0600

    add docker file

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Lablog API is running!");
+});
+
 app.use("/api/v1",routes);
 
 export default app;
